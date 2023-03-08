39fe77b58e18f16780952507d9489a1307064936
Mingxin Dong <mingxindong@Mingxins-MacBook-Air.local>
2023-03-08 12:01:28 +1000

    add basic layout route

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -24,6 +24,9 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @return void
      */
+    
+    protected $namespace =  'App\\Http\\Controllers';
+
     public function boot()
     {
         $this->configureRateLimiting();
@@ -34,6 +37,7 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
---
+++ app/helpers.php
@@ -0,0 +1,18 @@
+{
+    .
+    .
+    .
+    "autoload": {
+        "psr-4": {
+            "App\\": "app/",
+            "Database\\Factories\\": "database/factories/",
+            "Database\\Seeders\\": "database/seeders/"
+        },
+        "files": [
+            "app/helpers.php"
+        ]
+    },
+    .
+    .
+    .
+}
