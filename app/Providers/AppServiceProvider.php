c0fd76c0cc1c1cf60b0b5a2845ae091d281c6df2
Mingxin Dong <836514519dmx@gmail.com>
2023-03-21 07:01:04 +1000

    p

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,5 +29,6 @@ class AppServiceProvider extends ServiceProvider
         //     // 动态返回模型对应的策略名称，如：// 'App\Model\User' => 'App\Policies\UserPolicy',
         //     return 'App\Policies\\'.class_basename($modelClass).'Policy';
         // });
+        \Illuminate\Pagination\Paginator::useBootstrap();
     }
 }
